ec149aa016d14940607e70a0241082a6bde48e60
danrong <dl3333@columbia.edu>
2021-01-16 11:58:46 -0500

    add num

---
+++ module_3/cmake-build-debug/CMakeFiles/module_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_3.dir/main.cpp.o"
+  "module_3"
+  "module_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
