76a9b39bddf4ada71f65b73f8590c8f5879bc441
danrong <dl3333@columbia.edu>
2021-01-16 19:22:25 -0500

    \n

---
+++ module_4_part3_example1/cmake-build-debug/CMakeFiles/module_4_part3_example1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_4_part3_example1.dir/main.cpp.o"
+  "module_4_part3_example1"
+  "module_4_part3_example1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module_4_part3_example1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ module_4_part3_example1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char ch;
+    cout<<"hi"<<endl;
+    cout<<'\n';
+    cout<<endl;
+
+    cout<<"hi"<<endl;
+    ch = '\n';
+    cout<<ch;
+
+    cout<<"hi"<<endl;
+    cout<<"abc"<<'\n';
+    cout<<"abc\n";
+    cout<<"hi"<<endl;
+    return 0;
+}
