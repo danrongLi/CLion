5b718f3a8e20c54e1c79307f4444a49176c07495
danrong <dl3333@columbia.edu>
2021-01-16 19:53:53 -0500

    new int file

--- module4_dataTypes_int/cmake-build-debug/CMakeFiles/module_4.dir/DependInfo.cmake
+++ module4_dataTypes_int/cmake-build-debug/CMakeFiles/module_4.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/danrong/Desktop/CLion/module_4/main.cpp" "/Users/danrong/Desktop/CLion/module_4/cmake-build-debug/CMakeFiles/module_4.dir/main.cpp.o"
+  "/Users/danrong/Desktop/CLion/module4_dataTypes_int/main.cpp" "/Users/danrong/Desktop/CLion/module4_dataTypes_int/cmake-build-debug/CMakeFiles/module_4.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- module4_dataTypes_int/cmake-build-debug/cmake_install.cmake
+++ module4_dataTypes_int/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/danrong/Desktop/CLion/module_4
+# Install script for directory: /Users/danrong/Desktop/CLion/module4_dataTypes_int
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/danrong/Desktop/CLion/module_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/danrong/Desktop/CLion/module4_dataTypes_int/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
