7ed800c4c4deeb237a784b703430e098a93e5a0a
danrong <dl3333@columbia.edu>
2021-01-16 19:52:11 -0500

    new double file

---
+++ module4_dataTypes_double/cmake-build-debug/CMakeFiles/module_4_part2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_4_part2.dir/main.cpp.o"
+  "module_4_part2"
+  "module_4_part2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module_4_part2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
