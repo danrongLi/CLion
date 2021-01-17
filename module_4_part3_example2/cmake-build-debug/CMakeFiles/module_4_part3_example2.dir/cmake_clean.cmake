804739e51210195a18a50b6792934ce5ca89639a
danrong <dl3333@columbia.edu>
2021-01-16 19:25:54 -0500

    char

---
+++ module_4_part3_example2/cmake-build-debug/CMakeFiles/module_4_part3_example2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_4_part3_example2.dir/main.cpp.o"
+  "module_4_part3_example2"
+  "module_4_part3_example2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module_4_part3_example2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ module_4_part3_example2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char ch1, ch2;
+
+    ch1 = 'a';
+    ch2 = ch1+ 1;
+
+    cout<<ch2<<endl;
+    cout<<(char)('a'+1)<<endl;
+    cout<<('a'+1)<<endl;
+
+    return 0;
+}
