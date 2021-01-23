92e50487c069c8b1477801f1bed35e9031bbcabc
danrong <dl3333@columbia.edu>
2021-01-23 16:43:45 -0500

    hw3 question 4

---
+++ homework/hw3/dl4111_hw3_q4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+const int FLOOR_ROUND = 1;
+const int CEILING_ROUND = 2;
+const int ROUND = 3;
+const double NEAREST_INTEGER_THRESHOLD = 0.5;
+
+int main() {
+
+    double realNumber;
+    int roundingMethod;
+
+    cout<<"Please enter a Real number:"<<endl;
+    cin>>realNumber;
+
+    cout<<"Choose your rounding method:"<<endl;
+    cout<<"1. Floor round"<<endl;
+    cout<<"2. Ceiling round"<<endl;
+    cout<<"3. Round to the nearest whole number"<<endl;
+    cin>>roundingMethod;
+
+    switch (roundingMethod) {
+
+        case FLOOR_ROUND:
+            cout<<int(realNumber)<<endl;
+            break;
+
+        case CEILING_ROUND:
+            cout<<int(realNumber)+1<<endl;
+            break;
+
+        case ROUND:
+            if ((realNumber - int(realNumber)) >= NEAREST_INTEGER_THRESHOLD){
+                cout<<int(realNumber)+1<<endl;
+                break;
+            }
+            else if ((realNumber - int(realNumber)) < NEAREST_INTEGER_THRESHOLD){
+                cout<<int(realNumber)<<endl;
+                break;
+            }
+    }
+
+    return 0;
+}
