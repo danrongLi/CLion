919b24a5aec6ac159c183b7ade2f9fc601325583
danrong <dl3333@columbia.edu>
2021-01-26 15:10:40 -0500

    c++ book example

---
+++ module/module6_iterativeStatements/comments.cpp
@@ -0,0 +1,13 @@
+//File Name: comments.cpp
+//Author: Danrong Li
+//Email Address: dl4111@nyu.edu
+//Assignment Number: 2
+//Description: Program to show a standard comment to include.
+//Last Change: January 26th, 2021
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
