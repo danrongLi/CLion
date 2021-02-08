6179e9a7d84a3b1389a834ca5ce4f2da7726a576
danrongli <dl3333@columbia.edu>
2021-02-08 18:08:13 -0500

    gowtham practice exam q1 to q10

---
+++ practice exam/gowtham_exam1/dl4111_q1to10.cpp
@@ -0,0 +1,36 @@
+/*
+ * Danrong Li
+ * dl4111
+ *
+ * Question 1
+ * (a) 10010100
+ * (b) -108
+ *
+ * Question 2
+ * a
+ *
+ * Question 3
+ * b
+ *
+ * Question 4
+ * b
+ *
+ * Question 5
+ * a
+ *
+ * Question 6
+ * d
+ *
+ * Question 7
+ * d
+ *
+ * Question 8
+ * T F F T T F T F F T
+ *
+ * Question 9
+ * a
+ *
+ * Question 10
+ * {{4}, {1,4}, {2,4}, {1,2,4}}
+ *
+ */
