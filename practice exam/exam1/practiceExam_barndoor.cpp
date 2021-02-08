c5ef03cb9da8bea21653569825db4ff102afa98c
danrongli <dl3333@columbia.edu>
2021-02-08 12:23:18 -0500

    practice exam barn door

--- practice exam/exam1/practiceExam_barndoor.cpp
+++ practice exam/exam1/practiceExam_barndoor.cpp
@@ -1,3 +1,13 @@
+/*
+#######
+#$   $#
+# $ $ #
+#  $  #
+# $ $ #
+#$   $#
+#######
+ n = 7
+ */
 #include <iostream>
 using namespace std;
 
@@ -8,7 +18,7 @@ int main() {
     cout<<"Please enter an odd number which is also greater than equal to 3: "<<endl;
     cin>>n;
 
-    int lineCount, spaceCount, hashCount, dollarCount;
+    int lineCount, spaceCount, hashCount;
 
     for (lineCount = 1; lineCount <= (n+1)/2; lineCount += 1){
         if (lineCount == 1){
@@ -44,38 +54,33 @@ int main() {
         cout<<"\n";
     }
 
-        int botLineCount, botSpaceCount, botHashCount, botDollarCount;
-        for (botLineCount = 1; botLineCount <= (n+1)/2; botLineCount += 1){
-            if (botLineCount == (n+1)/2){
+        int botLineCount, botSpaceCount, botHashCount;
+        for (botLineCount = 1; botLineCount <= (n-1)/2; botLineCount += 1) {
+
+            if (botLineCount == (n-1)/2){
                 for (botHashCount = 1; botHashCount <= n; botHashCount += 1){
                     cout<<"#";
                 }
             }
-            else{
+
+            else {
                 cout<<"#";
-                botSpaceCount = botLineCount;
-                while (botSpaceCount >= 0){
+                for (botSpaceCount = 1; botSpaceCount <= (n-1)/2-botLineCount-1; botSpaceCount += 1){
                     cout<<" ";
-                    botSpaceCount -= 1;
                 }
                 cout<<"$";
 
-                for (botSpaceCount = 1; botSpaceCount <= (n-2*(botLineCount-2)-4); botSpaceCount += 1){
+                for (botSpaceCount = 1; botSpaceCount <= ((n+1)/2-(n-2*(botLineCount-1)-(n+1)/2)); botSpaceCount+=1){
                     cout<<" ";
                 }
-                if (botLineCount == (n+1)/2){
-                    cout<<"";
-                }
-                else{
-                    cout<<"$";
-                }
-                botSpaceCount = botLineCount - 2;
-                while (botSpaceCount > 0){
+                cout<<"$";
+
+                for (botSpaceCount = 1; botSpaceCount <= (n-1)/2-botLineCount-1; botSpaceCount += 1){
                     cout<<" ";
-                    botSpaceCount -= 1;
                 }
                 cout<<"#";
             }
+
             cout<<"\n";
 
         }
