bee119540a8af49696af1231928c18339b78d198
danrongli <dl3333@columbia.edu>
2021-02-13 20:34:43 -0500

    module 8 factorial kcombs functions

--- module/module8_functions/factorial_kcomb_function.cpp
+++ module/module8_functions/factorial_kcomb_function.cpp
@@ -3,24 +3,29 @@
 using namespace std;
 
 int factorial (int num);
+int kCombinations (int n, int k);
 
 int main() {
 
-    int n, k, i, nFact, kFact, n_kFact, kComb;
+    int n, k, i,  kComb;
     cout<<"Please n and k, n >= k:"<<endl;
     cin>>n>>k;
 
-    nFact = factorial (n);
-    kFact = factorial (k);
-    n_kFact = factorial (n-k);
-
-
-    kComb = nFact / (kFact * n_kFact);
+    kComb = kCombinations(n, k);
 
     cout<<n<<" choose "<<k<<" is "<<kComb;
 
     return 0;
 }
+
+int kCombinations (int n, int k){
+    int nFact, kFact, n_kFact;
+    nFact = factorial (n);
+    kFact = factorial (k);
+    n_kFact = factorial (n-k);
+    return (nFact/(kFact*n_kFact));
+}
+
 
 int factorial (int num){
     int factRes, i;
