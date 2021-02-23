fb357f35a0de85cec8d34e984d4756aa01af089c
danrongli <dl3333@columbia.edu>
2021-02-23 18:16:21 -0500

    module 10 strings

---
+++ module/module10_strings/compareStrings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string word1, word2, word3;
+    cout<<"Please enter 3 words separated by a space:"<<endl;
+    cin>>word1>>word2>>word3;
+
+    if ((word1 <= word2) && (word1 <= word3)){
+        cout<<word1<<endl;
+    }
+    else if ((word2 <= word1) && (word2 <= word3)){
+        cout<<word2<<endl;
+    }
+    else {
+        cout<<word3<<endl;
+    }
+
+    return 0;
+}
+
