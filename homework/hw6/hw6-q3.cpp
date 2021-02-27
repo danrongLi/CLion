60a012b857f09f1370607ec961c15f1a0612f2c6
danrongli <dl3333@columbia.edu>
2021-02-27 13:34:00 -0500

    hw6 q3 redo

---
+++ homework/hw6/hw6-q3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+double eApprox(int n);
+
+int main() {
+
+    cout.precision(30);
+
+    for (int n = 1; n <= 15; n += 1){
+        cout<<"n = "<<n<<'\t'<<eApprox(n)<<endl;
+    }
+    return 0;
+}
+
+double eApprox(int n){
+
+    double currentTerm;
+    double sumTerms = 0.0, numerator = 1.0, currentDenominator = 1.0;
+    int count;
+
+    for (count = 1; count <= (n+1); count += 1){
+        currentTerm = numerator / currentDenominator;
+        sumTerms +=  currentTerm;
+        currentDenominator *= count;
+    }
+    return sumTerms;
+}
