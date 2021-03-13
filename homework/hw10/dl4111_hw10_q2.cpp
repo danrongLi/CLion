f07bc25173ebce3e7bf3e18e2ae8bddbf3137e56
danrongli <dl3333@columbia.edu>
2021-03-13 12:25:56 -0500

    hw10 question 2

---
+++ homework/hw10/dl4111_hw10_q2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int* findMissing(int arr[], int n, int& resArrSize);
+
+int main() {
+
+    int testArr[10] = {3,1,3,0,6,4};
+    int testN = 6;
+    int reArrSize;
+    int* missingArr = findMissing(testArr, testN, reArrSize);
+
+    cout<<"new size: "<<reArrSize<<endl;
+
+    cout<<"array: ";
+
+    for (int i = 0; i < reArrSize; i += 1){
+        cout<<missingArr[i]<<" ";
+    }
+    cout<<endl;
+
+    delete [] missingArr;
+    missingArr = NULL;
+
+    return 0;
+}
+
+int* findMissing(int arr[], int n, int& resArrSize){
+    int* missingArr = NULL;
+    int* allArr =  new int[n+1];
+    for (int i = 0; i <= n; i += 1){
+        allArr[i] = i;
+    }
+
+    int missingCount = n+1;
+    for (int i = 0; i < n; i += 1){
+        if (allArr[arr[i]] != -1){
+            allArr[arr[i]] = -1;
+            missingCount -= 1;
+        }
+    }
+
+
+    missingArr = new int[missingCount];
+    int missingIndex = 0;
+    for ( int i = 0; i <= n; i += 1){
+        if (allArr[i] != -1){
+            missingArr[missingIndex] = allArr[i];
+            missingIndex += 1;
+        }
+    }
+
+    delete [] allArr;
+    allArr = NULL;
+
+    resArrSize = missingCount;
+    return missingArr;
+
+
+}
+
