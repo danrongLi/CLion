afadf6192bd2c400e0f595e0a4f7d5cfe16e84f3
danrongli <dl3333@columbia.edu>
2021-03-19 13:51:38 -0400

    hw11 question 2

---
+++ homework/hw11/dl4111_hw11_q2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>//;
+using namespace std;
+
+int sumOfSquares(int arr[], int arrSize);
+bool isSorted(int arr[], int arrSize);
+
+int main() {
+
+    int arr[10] = {2, -1 , 3, 10};
+    int arrSize = 4;
+
+    int sumSqr = sumOfSquares(arr, arrSize);
+    cout<<"part a, sum of squares is: "<<sumSqr<<endl;
+
+    bool ifSorted = isSorted(arr, arrSize);
+    if (ifSorted){
+        cout<<"part b, elements are sorted in ascending order"<<endl;
+    }
+    else {
+        cout<<"part b, elements are NOT sorted in ascending order"<<endl;
+    }
+
+    return 0;
+}
+int sumOfSquares(int arr[], int arrSize){
+    int currentSumOfSquare;
+    if (arrSize == 1){
+        return pow(arr[0],2);
+    }
+    else {
+        currentSumOfSquare = sumOfSquares(arr, arrSize-1);
+        currentSumOfSquare += pow(arr[arrSize-1], 2);
+        return currentSumOfSquare;
+    }
+}
+bool isSorted(int arr[], int arrSize){
+    bool ifSorted;
+    if (arrSize == 1){
+        return true;
+    }
+    else {
+        ifSorted = isSorted(arr, arrSize-1);
+        if (!ifSorted){
+            return false;
+        }
+        else {
+            if (arr[arrSize-1] >= arr[arrSize-2]){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
