0a739a12263bd516b0917c7299b832d0427f71e5
danrongli <dl3333@columbia.edu>
2021-03-22 12:20:25 -0400

    hw11 question 1

--- homework/hw11/dl4111_hw11_q1.cpp
+++ homework/hw11/dl4111_hw11_q1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void printTriangle(int n);
-void printOppositeTriangles(int n);
+void printOpositeTriangles(int n);
 void printRuler(int n);
 
 int main() {
@@ -10,7 +10,7 @@ int main() {
     cout<<"part a:"<<endl;
     printTriangle(4);
     cout<<"part b:"<<endl;
-    printOppositeTriangles(4);
+    printOpositeTriangles(4);
     cout<<"part c:"<<endl;
     printRuler(4);
 
@@ -30,7 +30,7 @@ void printTriangle(int n){
     cout<<endl;
 }
 
-void printOppositeTriangles(int n){
+void printOpositeTriangles(int n){
     if (n == 0){
         return;
     }
@@ -39,7 +39,7 @@ void printOppositeTriangles(int n){
             cout<<"*";
         }
         cout<<endl;
-        printOppositeTriangles(n-1);
+        printOpositeTriangles(n - 1);
         for (int i = 0; i <= (n-1); i += 1){
             cout<<"*";
         }
