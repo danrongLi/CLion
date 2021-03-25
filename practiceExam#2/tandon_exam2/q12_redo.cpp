2e7191a31ca5522366f147cf6a36f9cfbfb59088
danrongli <dl3333@columbia.edu>
2021-03-25 13:50:11 -0400

    q12 redo again

--- practiceExam#2/tandon_exam2/q12_redo.cpp
+++ practiceExam#2/tandon_exam2/q12_redo.cpp
@@ -6,8 +6,10 @@ int maxZeroLength(int nums[], int len, int startIdx);
 
 int main() {
 
-    int nums[10] = {0,0,1,0,0,0};
-    int len = 6;
+    int nums[10] = {0,0,0,0,1,0,1,0,0,1};
+
+    int len = 10;
+
     int startIdx = 0;
 
     int result = maxZeroLength(nums, len, startIdx);
@@ -20,16 +22,20 @@ int maxZeroLength(int nums[], int len, int startIdx){
     int currentCount = 0;
     int currentIndex = startIdx;
 
-    while (nums[currentIndex] != 0 && currentIndex < len){
-        currentIndex += 1;
-    }
-    while (nums[currentIndex] == 0 && currentIndex < len){
-        currentCount += 1;
-        currentIndex += 1;
-    }
     if (currentIndex >= len){
         return currentCount;
     }
-    return std::max(currentCount, maxZeroLength(nums, len, currentIndex));
+    else {
+        while (nums[currentIndex] != 0 && currentIndex < len){
+            currentIndex += 1;
+        }
+        while (nums[currentIndex] == 0 && currentIndex < len){
+            currentCount += 1;
+            currentIndex += 1;
+        }
+
+        return std::max(currentCount, maxZeroLength(nums, len, currentIndex));
+    }
+
 
 }
