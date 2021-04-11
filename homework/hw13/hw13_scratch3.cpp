5091cd860a0d4a95918177c6fa330b13b7462df3
danrongli <dl3333@columbia.edu>
2021-04-11 12:15:53 -0400

    hw13 cant solve it

--- homework/hw13/hw13_scratch3.cpp
+++ homework/hw13/hw13_scratch3.cpp
@@ -11,6 +11,10 @@ const int BIG_IDX = SIZE - 1;
 const int INITIAL_ANT = 100;
 const int INITIAL_DOODLE = 5;
 
+class Organism;
+class Ant;
+class Doodlebug;
+
 class Point{
 public:
     Point():x(0),y(0),ifValid(true), ifOccupied(false), whetherAnt(false){}
@@ -18,7 +22,7 @@ public:
 
     int getX() const {return x;}
     int getY() const {return y;}
-    bool getIfValid() const {return ifValid;};
+    bool getIfValid() const {return ifValid;}
     bool getIfOccupied() const {return ifOccupied;}
     bool getWhetherAnt() const {return whetherAnt;}
 
@@ -28,6 +32,14 @@ public:
     void setToOccupied(){ifOccupied = true;} //call to make it occupied
     void setToHaveAnt(){whetherAnt = true;} //call to make it ANT
 
+    void setToNotOccupied(){ifOccupied = false;} //call to make it not occupied
+    void setToNotAnt(){whetherAnt = false;} //call to make it non-ANT
+
+    void setIfOccupied(Organism* world[SIZE][SIZE]);
+
+    friend class Organism;
+    friend class Ant;
+    friend class Doodlebug;
 
 private:
     int x;
@@ -36,6 +48,15 @@ private:
     bool ifOccupied;
     bool whetherAnt;
 };
+void Point::setIfOccupied(Organism *(*world)[20]) {
+    if(world[x][y] == nullptr){
+        ifOccupied = false;
+    }
+    else{
+        ifOccupied = true;
+    }
+}
+
 void Point::setIfValid() {
     if (x <= BIG_IDX && x >= SMALL_IDX && y <= BIG_IDX && y >= SMALL_IDX && (!ifOccupied)){
         ifValid = true;
@@ -49,9 +70,9 @@ void Point::setIfValid() {
 class Organism{
 public:
     virtual void move() = 0;
-    virtual ~Organism() = default;
-    Organism():location(), daysSurvived(0), daysSinceLastBreed(0), readyToBreed(false), ifDead(false){}
-    Organism(int x, int y): location(x,y),daysSurvived(0), daysSinceLastBreed(0), readyToBreed(false), ifDead(false){}
+    virtual ~Organism()=default;
+    Organism():location(), daysSurvived(0), daysSinceLastBreed(0), readyToBreed(false), ifDead(false), type('-'), whetherAnt(false), ifOccupied(false), alreadyPlaced(false){}
+    Organism(int x, int y): location(x,y),daysSurvived(0), daysSinceLastBreed(0), readyToBreed(false), ifDead(false), type('-'),  whetherAnt(false), ifOccupied(false), alreadyPlaced(false){}
 
     Point getLocation() const {return location;}
     void setLocation(int x, int y){location.setX(x); location.setY(y);}
@@ -71,6 +92,18 @@ public:
     Point getNextLocation() const{return nextLocation;}
     void setNextLocation();
 
+    void setToOccupied(){ifOccupied = true;} //call to make it occupied
+    void setToHaveAnt(){whetherAnt = true;} //call to make it ANT
+
+    void setToNotOccupied(){ifOccupied = false;} //call to make it not occupied
+    void setToNotAnt(){whetherAnt = false;} //call to make it non-ANT
+
+    bool getIfOccupied() const {return ifOccupied;}
+    bool getWhetherAnt() const {return whetherAnt;}
+
+    bool getIfPlace() const {return alreadyPlaced;}
+    void setToPlace() {alreadyPlaced = true;}
+
 private:
     Point location;
     int daysSurvived;
@@ -79,6 +112,11 @@ private:
     bool ifDead;
     char type;
     Point nextLocation;
+
+    bool whetherAnt;
+    bool ifOccupied;
+
+    bool alreadyPlaced;
 };
 void Organism::setNextLocation() {
     int currentX = location.getX();
@@ -94,106 +132,116 @@ void Organism::setNextLocation() {
 
     if (type == 'X'){
         if (up.getIfValid() && up.getWhetherAnt()){
+            cout<<"Found ant"<<endl;
             nextLocation.setX(currentX);
             nextLocation.setY(currentY+1);
-//            location.setToOccupied();
         }
         else if (down.getIfValid() && down.getWhetherAnt()){
+            cout<<"Found ant"<<endl;
             nextLocation.setX(currentX);
             nextLocation.setY(currentY-1);
-//            location.setToOccupied();
         }
         else if (left.getIfValid() && left.getWhetherAnt()){
+            cout<<"Found ant"<<endl;
             nextLocation.setX(currentX-1);
             nextLocation.setY(currentY);
-//            location.setToOccupied();
         }
         else if (right.getIfValid() && right.getWhetherAnt()){
+            cout<<"Found ant"<<endl;
             nextLocation.setX(currentX+1);
             nextLocation.setY(currentY);
-//            location.setToOccupied();
         }
         else {
-            srand(time(0));
             int choice = rand()%4+1;
             if (choice == 1){
                 if (up.getIfValid()){
+                    nextLocation.setX(currentX);
                     nextLocation.setY(currentY+1);
-//                    location.setToOccupied();
+                }
+                else {
+                    nextLocation.setX(currentX);
+                    nextLocation.setY(currentY);
                 }
             }
             else if (choice == 2){
+
                 if (down.getIfValid()){
+                    nextLocation.setX(currentX);
                     nextLocation.setY(currentY-1);
-//                    location.setToOccupied();
+                }
+                else {
+                    nextLocation.setX(currentX);
+                    nextLocation.setY(currentY);
                 }
             }
             else if (choice == 3){
 
                 if (left.getIfValid()){
                     nextLocation.setX(currentX-1);
-//                    location.setToOccupied();
+                    nextLocation.setY(currentY);
+                }
+                else {
+                    nextLocation.setX(currentX);
+                    nextLocation.setY(currentY);
                 }
             }
             else if (choice == 4){
 
                 if (right.getIfValid()){
                     nextLocation.setX(currentX+1);
-//                    location.setToOccupied();
+                    nextLocation.setY(currentY);
                 }
+                else {
+                    nextLocation.setX(currentX);
+                    nextLocation.setY(currentY);
+                }
+            }
+        }
+    }
+    else if (type == 'O'){
+        int choice = rand()%4+1;
+        if (choice == 1){
+            if (up.getIfValid()){
+                nextLocation.setX(currentX);
+                nextLocation.setY(currentY+1);
             }
             else {
                 nextLocation.setX(currentX);
                 nextLocation.setY(currentY);
             }
         }
-    }
-    else if (type == 'O'){
-        srand(time(0));
-        int choice = rand()%4+1;
-        if (choice == 1){
-
-            if (up.getIfValid()){
-                nextLocation.setY(currentY+1);
-//            location.setToOccupied();
-//            if (type == 'O'){
-//                location.setToHaveAnt();
-//            }
-            }
-        }
         else if (choice == 2){
 
             if (down.getIfValid()){
+                nextLocation.setX(currentX);
                 nextLocation.setY(currentY-1);
-//            location.setToOccupied();
-//            if (type == 'O'){
-//                location.setToHaveAnt();
-//            }
+            }
+            else {
+                nextLocation.setX(currentX);
+                nextLocation.setY(currentY);
             }
         }
         else if (choice == 3){
 
             if (left.getIfValid()){
                 nextLocation.setX(currentX-1);
-//            location.setToOccupied();
-//            if (type == 'O'){
-//                location.setToHaveAnt();
-//            }
+                nextLocation.setY(currentY);
+            }
+            else {
+                nextLocation.setX(currentX);
+                nextLocation.setY(currentY);
             }
         }
         else if (choice == 4){
 
             if (right.getIfValid()){
                 nextLocation.setX(currentX+1);
-//            location.setToOccupied();
-//            if (type == 'O'){
-//                location.setToHaveAnt();
-//            }
+                nextLocation.setY(currentY);
             }
-        }
-        else {
-            nextLocation.setX(currentX);
-            nextLocation.setY(currentY);
+            else {
+                nextLocation.setX(currentX);
+                nextLocation.setY(currentY);
+            }
         }
     }
     else {
@@ -205,36 +253,60 @@ void Organism::setNextLocation() {
 }
 class Ant: public Organism{
 public:
-    Ant():Organism(){setType('O');}
-    Ant(int x, int y):Organism(x,y){setType('O');}
+    friend class Point;
+    Ant():Organism(){setType('O');setToHaveAnt(); setToOccupied();}
+    Ant(int x, int y):Organism(x,y){setType('O');setToHaveAnt(); setToOccupied();}
 
     virtual void move();
 private:
-
 };
 void Ant::move() {
+    setNextLocation();
     Point next = getNextLocation();
+//    cout<<"old location: "<<getLocation().getX()<<" "<<getLocation().getY()<<endl;
+//    cout<<"proposed next location: "<<next.getX()<<" "<<next.getY()<<endl;
     int currentSurvived = getDaysSurvived();
     int getBreed = getDaysSinceLastBreed();
-//    more to come..
+    setToNotOccupied();
+    setToNotAnt();
+    setLocation(next.getX(), next.getY());
+    setToOccupied();
+    setToHaveAnt();
+    setDaysSurvived(currentSurvived+1);
+    setDaysSinceLastBreed(getBreed+1);
+//    Add breed things
+//    cout<<"new location: "<<getLocation().getX()<<" "<<getLocation().getY()<<endl;
 }
 
 class Doodlebug : public Organism{
 public:
-    Doodlebug():Organism(), whetherJustAte(false){setType('X');}
-    Doodlebug(int x, int y):Organism(x,y), whetherJustAte(false){setType('X');}
+    friend class Point;
+    Doodlebug():Organism(), whetherJustAte(false){setType('X');setToOccupied();}
+    Doodlebug(int x, int y):Organism(x,y), whetherJustAte(false){setType('X');setToOccupied();}
     virtual void move();
 private:
     bool whetherJustAte;
 };
 void Doodlebug::move() {
-
+    setNextLocation();
+    Point next = getNextLocation();
+//    cout<<"old location: "<<getLocation().getX()<<" "<<getLocation().getY()<<endl;
+//    cout<<"proposed next location: "<<next.getX()<<" "<<next.getY()<<endl;
+    int currentSurvived = getDaysSurvived();
+    int getBreed = getDaysSinceLastBreed();
+    setToNotOccupied();
+    setLocation(next.getX(), next.getY());
+    setToOccupied();
+    setDaysSurvived(currentSurvived+1);
+    setDaysSinceLastBreed(getBreed+1);
+//    cout<<"new location: "<<getLocation().getX()<<" "<<getLocation().getY()<<endl;
+//    Add breed things
 }
 void printWorld(Organism* world[SIZE][SIZE]){
     for (int i = 0; i < SIZE; i += 1){
         for (int j = 0; j < SIZE; j += 1){
             if (world[i][j] != nullptr){
-                cout<<world[i][j]->getType()<<" ";
+                world[i][j]->display();
             }
             else {
                 cout<<"- ";
@@ -242,8 +314,8 @@ void printWorld(Organism* world[SIZE][SIZE]){
         }
         cout<<endl;
     }
-    cout<<endl;
 }
+
 int main(){
     srand(time(0));
     Organism* world[SIZE][SIZE];
@@ -257,21 +329,18 @@ int main(){
     while (initialDoodle < INITIAL_DOODLE){
         int x = rand()%SIZE;
         int y = rand()%SIZE;
-        Doodlebug tempDoodle(x,y);
         if (world[x][y] == nullptr){
-            tempDoodle.getLocation().setToOccupied();
-            world[x][y] = &tempDoodle;
+            world[x][y] = new Doodlebug(x,y);
+
             initialDoodle += 1;
         }
     }
     while (initialAnt < INITIAL_ANT){
     int x = rand()%SIZE;
     int y = rand()%SIZE;
-    Ant tempAnt(x,y);
     if (world[x][y] == nullptr){
-        tempAnt.getLocation().setToOccupied();
-        tempAnt.getLocation().setToHaveAnt();
-        world[x][y] = &tempAnt;
+        world[x][y] = new Ant(x,y);
+
         initialAnt += 1;
     }
 }
@@ -282,12 +351,55 @@ int main(){
     string s;
     getline(cin, s);
     int round = 1;
-    cout<<"Round "<<round<<endl<<endl;
+    cout<<"Round "<<round;
+
     for (int i = 0; i < SIZE; i += 1){
         for (int j = 0; j < SIZE; j += 1){
-            world[i][j]->move();
+            if (world[i][j] != nullptr && world[i][j]->getType() == 'X'){
+                int newX = i;
+                int newY = j;
+//                cout<<"old location "<<i<<" "<<j<<endl;
+                world[i][j]->move();
+                if(world[newX][newY] != nullptr && !world[newX][newY]->getIfPlace()){
+                    newX = world[i][j]->getLocation().getX();
+                    newY = world[i][j]->getLocation().getY();
+                    if(world[newX][newY] != nullptr && world[newX][newY]->getLocation().getIfValid()){
+                        world[newX][newY] = world[i][j];
+                        world[newX][newY]->setToPlace();
+                        world[i][j] = nullptr;
+                    }
+//                    cout<<"mew location "<<newX<<" "<<newY<<endl;
+//                    world[newX][newY] = world[i][j];
+//                    world[newX][newY]->setToPlace();
+//                    world[i][j] = nullptr;
+                }
+            }
         }
     }
+
+    for (int i = 0; i < SIZE; i += 1){
+        for (int j = 0; j < SIZE; j += 1){
+            if (world[i][j] != nullptr && world[i][j]->getType() == 'O'){
+                int newX = i;
+                int newY = j;
+                world[i][j]->move();
+                if(world[newX][newY] != nullptr && !world[newX][newY]->getIfPlace()){
+                    newX = world[i][j]->getLocation().getX();
+                    newY = world[i][j]->getLocation().getY();
+                    if(world[newX][newY] != nullptr && world[newX][newY]->getLocation().getIfValid()){
+                        world[newX][newY] = world[i][j];
+                        world[newX][newY]->setToPlace();
+                        world[i][j] = nullptr;
+                    }
+//                    cout<<"mew location "<<newX<<" "<<newY<<endl;
+//                    world[newX][newY] = world[i][j];
+//                    world[newX][newY]->setToPlace();
+//                    world[i][j] = nullptr;
+                }
+            }
+        }
+    }
+    cout<<" with Ant# = "<<" "<<" Doodlebug# = "<<" "<<endl<<endl;
     printWorld(world);
 
 
