ffa813f827133b4ae2a42bac739e43df71144fe5
danrongli <dl3333@columbia.edu>
2021-04-30 18:44:33 -0400

    hw16 finished basic balancing

--- homework/hw16/checkBalance.cpp
+++ homework/hw16/checkBalance.cpp
@@ -3,11 +3,25 @@
 #include <string>
 #include <fstream>
 #include <stack>
-
 using namespace std;
 
+
+template <class T>
+class Stack{
+public:
+    void push(T newItem) {data.push_front(newItem);}
+    void pop() {data.pop_front();}
+    T top() const {return *data.begin();}
+    bool isEmpty() const {return data.empty();}
+    int size() const {return data.size();}
+    void clear() {data.clear();}
+private:
+    list<T> data;
+};
+
+
 bool isBalance(const string& expression){
-    stack<char> s;
+    Stack<char> s;
     if (expression.empty()){
         cout<<"You gave me an empty list! I guess it should be balanced!"<<endl;
         return true;
@@ -16,7 +30,7 @@ bool isBalance(const string& expression){
         if (expression[i] == '{' || expression[i] == '[' || expression[i] == '('){
             s.push(expression[i]);
         }
-        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (!s.empty())){
+        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (!s.isEmpty())){
             char poppedChar = s.top();
             s.pop();
             switch (expression[i]) {
@@ -37,20 +51,66 @@ bool isBalance(const string& expression){
                     break;
             }
         }
-        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (s.empty())){
+        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (s.isEmpty())){
             return false;
         }
         else{
             continue;
         }
     }
-    if (!s.empty()){
+    if (!s.isEmpty()){
         return false;
     }
     else {
         return true;
     }
 }
+
+//bool isBalance(const string& expression){
+//    stack<char> s;
+//    if (expression.empty()){
+//        cout<<"You gave me an empty list! I guess it should be balanced!"<<endl;
+//        return true;
+//    }
+//    for (int i = 0; i < expression.size(); i += 1){
+//        if (expression[i] == '{' || expression[i] == '[' || expression[i] == '('){
+//            s.push(expression[i]);
+//        }
+//        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (!s.empty())){
+//            char poppedChar = s.top();
+//            s.pop();
+//            switch (expression[i]) {
+//                case '}':
+//                    if (poppedChar != '{'){
+//                        return false;
+//                    }
+//                    break;
+//                case ']':
+//                    if (poppedChar != '['){
+//                        return false;
+//                    }
+//                    break;
+//                case ')':
+//                    if (poppedChar != '('){
+//                        return false;
+//                    }
+//                    break;
+//            }
+//        }
+//        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (s.empty())){
+//            return false;
+//        }
+//        else{
+//            continue;
+//        }
+//    }
+//    if (!s.empty()){
+//        return false;
+//    }
+//    else {
+//        return true;
+//    }
+//}
 
 int main() {
     string expr = "if (x==y){k;x=z;} if (arr[3] != [v2]){return false;}";
--- homework/hw16/dl4111_hw16_q1.cpp
+++ homework/hw16/dl4111_hw16_q1.cpp
@@ -10,15 +10,60 @@ template <class T>
 class Stack{
 public:
     void push(T newItem) {data.push_front(newItem);}
-    T pop() {return data.pop_front();}
+    void pop() {data.pop_front();}
     T top() const {return *data.begin();}
-    bool isEmpty() const {return data.size() == 0;}
+    bool isEmpty() const {return data.empty();}
     int size() const {return data.size();}
     void clear() {data.clear();}
 private:
     list<T> data;
 };
 
+bool isBalance(const string& expression){
+    Stack<char> s;
+    if (expression.empty()){
+        cout<<"You gave me an empty list! I guess it should be balanced!"<<endl;
+        return true;
+    }
+    for (int i = 0; i < expression.size(); i += 1){
+        if (expression[i] == '{' || expression[i] == '[' || expression[i] == '('){
+            s.push(expression[i]);
+        }
+        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (!s.isEmpty())){
+            char poppedChar = s.top();
+            s.pop();
+            switch (expression[i]) {
+                case '}':
+                    if (poppedChar != '{'){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (poppedChar != '['){
+                        return false;
+                    }
+                    break;
+                case ')':
+                    if (poppedChar != '('){
+                        return false;
+                    }
+                    break;
+            }
+        }
+        else if ((expression[i] == '}' || expression[i] == ']' || expression[i] == ')') && (s.isEmpty())){
+            return false;
+        }
+        else{
+            continue;
+        }
+    }
+    if (!s.isEmpty()){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
 int main() {
 
