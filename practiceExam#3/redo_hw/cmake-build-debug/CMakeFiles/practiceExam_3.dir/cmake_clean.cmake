f388e680754a104f2e288dcf857919d1e25315da
danrongli <dl3333@columbia.edu>
2021-05-10 18:10:04 -0400

    re-organizing

---
+++ practiceExam#3/redo_hw/cmake-build-debug/CMakeFiles/practiceExam_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practiceExam_3.dir/hw17_redo.cpp.o"
+  "practiceExam_3"
+  "practiceExam_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practiceExam_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
