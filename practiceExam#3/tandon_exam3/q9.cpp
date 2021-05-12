43690624f472a7def75a18c018768e0381cf894f
danrongli <dl3333@columbia.edu>
2021-05-11 21:41:20 -0400

    tandon practice

--- practiceExam#3/tandon_exam3/q9.cpp
+++ practiceExam#3/tandon_exam3/q9.cpp
@@ -3,51 +3,219 @@
 #include <string>
 
 using namespace std;
+void swap(int& a, int& b);
 
 class Sorting{
 public:
-    virtual void sort(vector<int> data) = 0;
+    virtual void sort(vector<int>& data) = 0;
     virtual string worst_case_runtime() = 0;
 
-private:
+protected:
     vector<int> data;
 };
 
 class InsertionSort: public Sorting{
 public:
-    virtual void sort(vector<int> data);
+    virtual void sort(vector<int>& data);
     virtual string worst_case_runtime(){return "O(n^2)";}
 private:
 
 };
 
+void InsertionSort::sort(vector<int>& data) {
+    for (int iterator = 1; iterator < data.size(); iterator += 1){
+        int current = data[iterator];
+        int indexPred = iterator - 1;
+        while (indexPred >= 0 && data[indexPred] > current){
+            data[iterator] = data[indexPred];
+            iterator -= 1;
+            indexPred -= 1;
+        }
+        data[indexPred+1] = current;
+    }
+    for (int i = 0; i < data.size(); i += 1){
+        cout<<data[i]<<" ";
+    }
+    cout<<endl;
+}
+
 class QuickSort: public Sorting{
 public:
-    virtual void sort(vector<int> data);
+    virtual void sort(vector<int>& data){quickSort(data, 0, data.size()-1);}
     virtual string worst_case_runtime(){return "O(n^2)";}
+
+    void quickSort(vector<int>& data, int low, int high){
+        if (low < high){
+            int partitionIndex = partition(data, low, high);
+            quickSort(data, 0, partitionIndex-1);
+            quickSort(data, partitionIndex+1, high);
+        }
+    }
+
+    int partition(vector<int>& test, int low, int high){
+        int i = low-1;
+        int pivot = test[high];
+        for (int j = low; j <= high-1; j += 1){
+            if(test[j] <= pivot){
+                i += 1;
+                swap(test[i],test[j]);
+            }
+        }
+        swap(test[i+1], test[high]);
+        return i+1;
+    }
 private:
 };
 
 class MergeSort: public Sorting{
 public:
-    virtual void sort(vector<int> data);
+    virtual void sort(vector<int>& data){mergeSort(data, 0, data.size()-1);}
     virtual string worst_case_runtime(){return "O(n*(log(n)))";}
+
+    void mergeSort(vector<int>& data, int low, int high){
+        if (low < high){
+            int middle = (high+low)/2;
+            mergeSort(data, low, middle);
+            mergeSort(data, middle+1, high);
+            merge(data, low, middle, high);
+        }
+    }
+    void merge(vector<int>& data, int low, int middle, int high){
+        int n1 = middle - low + 1;
+        int n2 = high - middle;
+
+        int L[n1], R[n2];
+        for (int i = 0; i < n1; i++)
+            L[i] = data[low + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = data[middle + 1 + j];
+
+        int i = 0;
+        int j = 0;
+        int index = low;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                data[index] = L[i];
+                i++;
+                index++;
+            }
+            else {
+                data[index] = R[j];
+                j++;
+                index++;
+            }
+
+        }
+        while (i < n1) {
+            data[index] = L[i];
+            i++;
+            index++;
+        }
+        while (j < n2) {
+            data[index] = R[j];
+            j++;
+            index++;
+        }
+    }
 private:
 };
 
-void InsertionSort::sort(vector<int> data) {
+class BubbleSort : public Sorting{
+public:
+    virtual void sort(vector<int>& data){
+        bool swapHappened;
+        do{
+            swapHappened = false;
+            for (int i = 1; i < data.size(); i += 1){
+                if (data[i-1] > data[i]){
+                    swap(data[i-1], data[i]);
+                    swapHappened = true;
+                }
+            }
+        }while(swapHappened);
+    }
+    virtual string worst_case_runtime() {return "O(n^2)";}
+private:
+};
 
-}
-
-void QuickSort::sort(vector<int> data) {
-
-}
-
-void MergeSort::sort(vector<int> data) {
-
-}
+class SelectionSort : public Sorting{
+public:
+    virtual void sort(vector<int>& data){
+        int sortedIndex = 0;
+        int unsortedIndex = 0;
+        while (sortedIndex < data.size()){
+            int currentMin = data[unsortedIndex];
+            int minIndex;
+            for (int i = unsortedIndex+1; i < data.size(); i += 1){
+                if (data[i] < currentMin){
+                    currentMin = data[i];
+                    minIndex = i;
+                }
+            }
+            swap(data[minIndex],data[sortedIndex]);
+            sortedIndex += 1;
+            unsortedIndex += 1;
+        }
+    }
+    virtual string worst_case_runtime() {return "O(n^2)";}
+private:
+};
 
 
 int main(){
+    vector<int> test = {4,3,2,10,12,1,5,6};
+    vector<int> test2 = {4,3,2,10,12,1,5,6};
+    vector<int> test3 = {4,3,2,10,12,1,5,6};
+    vector<int> test4 = {4,3,2,10,12,1,5,6};
+    vector<int> test5 = {4,3,2,10,12,1,5,6};
+
+    InsertionSort insertionTest;
+    cout<<"Insertion Sort"<<endl;
+    insertionTest.sort(test);
+    cout<<insertionTest.worst_case_runtime()<<endl;
+
+    QuickSort quickTest;
+    cout<<"Quick Sort"<<endl;
+    quickTest.sort(test2);
+    for (int i = 0; i < test2.size(); i += 1){
+        cout<<test2[i]<<" ";
+    }
+    cout<<endl;
+    cout<<quickTest.worst_case_runtime()<<endl;
+
+    MergeSort mergeTest;
+    cout<<"Merge Sort"<<endl;
+    mergeTest.sort(test3);
+    for (int i = 0; i < test3.size(); i += 1){
+        cout<<test3[i]<<" ";
+    }
+    cout<<endl;
+    cout<<mergeTest.worst_case_runtime()<<endl;
+
+    BubbleSort bubbleTest;
+    cout<<"Bubble Sort"<<endl;
+    bubbleTest.sort(test4);
+    for (int i = 0; i < test4.size(); i += 1){
+        cout<<test4[i]<<" ";
+    }
+    cout<<endl;
+    cout<<bubbleTest.worst_case_runtime()<<endl;
+
+    SelectionSort selectTest;
+    cout<<"Selection Sort"<<endl;
+    selectTest.sort(test5);
+    for (int i = 0; i < test5.size(); i += 1){
+        cout<<test5[i]<<" ";
+    }
+    cout<<endl;
+    cout<<selectTest.worst_case_runtime()<<endl;
+
     return 0;
+}
+
+void swap(int& a, int& b){
+    int t = a;
+    a = b;
+    b = t;
 }
--- practiceExam#3/tandon_exam3/short_answers.cpp
+++ practiceExam#3/tandon_exam3/short_answers.cpp
@@ -1,7 +1,7 @@
 /*
  * Question 1
  *
- * Given a nearly sorted vector of integers, I would prefer to use quick sort.
+ * Given a nearly sorted vector of integers, I would prefer to use insertion sort.
  *
  * Insertion sort means splitting the array into a sorted and unsorted part,
  * picking integers from unsorted part and placing it in the correct position in the sorted part.
@@ -9,26 +9,29 @@
  * Quick sort means selecting a pivot, and putting smaller than pivot elements before pivot,
  * putting greater than pivot elements after x.
  *
- * Since we have a vector of integers that are nearly sorted, quick sort would be better.
- * We don't need to force sorted order every step like with insertion sort.
+ * Insertion is faster in this case, it goes from O(n^2) to O(n) in this situation.
+ *
+ * (selection means finding the min, so still need O(n^2))
+ * (insertion, selection, merge, quick sort)
  *
  * Question 2
  *
  * In the worst case, quick sort has time complexity of O(n^2).
  *
- * This is due to the poor choice of the pivot. If the pivot is chosen as the first element,
- * which happens to be the smallest. The other elements would just all be placed after pivot, which is doing nothing.
+ * Suppose we have a list that is nearly sorted. If our pivot chosen is either the first or last element of the list,
+ * we'd have n groups that each each need to be iterated through n times.
  *
  * Question 3
  *
  * The drawback in space efficiency for merge sort is that it requires an extra array to store the merged array.
+ * (need auxiliary array for the merge step)
  * The advantage that merge sort has in terms of time efficiency over quicksort is that
- * its worst case is O(n*log(n))
+ * it's always O(nlog(n))
  *
  * Question 4
  *
  * The primary assumption that a developer has to make before using a binary search algorithm is that
- * there are no duplicates.
+ * data has to sorted.
  *
  * Question 5
  *
@@ -42,7 +45,7 @@
  *
  * Question 6
  *
- * Making a class abstract means the class has virtual functions.
+ * Making a class abstract means the class has pure virtual functions.
  * We cannot create an instance of an abstract class.
  *
  */
@@ -60,9 +63,8 @@
 /*
  * Question 7
  *
- * Encapsulation means the private members of the class can only be accessed by its member functions or friend classes.
- *
- * If other classes want to access the private data, they have to go through the accessors & mutators.
+ * Encapsulation is the idea of hiding data and functions within a single unit.
+ * In C++, we can do this via classes.
  *
  * Question 8
  *
