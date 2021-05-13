d70be7f5566758f3b2c1adb02a5a6f90a7041c97
danrongli <dl3333@columbia.edu>
2021-05-12 23:05:54 -0400

    azure practice

--- practiceExam#3/azure_exam3/q11.cpp
+++ practiceExam#3/azure_exam3/q11.cpp
@@ -3,24 +3,74 @@
 
 using namespace std;
 
-class Node;
+
+class Node{
+public:
+    Node(Node* newL = nullptr, Node* newR = nullptr){left = newL; right = newR;}
+    virtual double calculateValue() = 0;
+
+    Node* getLeft() {return left;}
+    Node* getRight(){return right;}
+    void setLeft(Node* newL){left = newL;}
+    void setRight(Node* newR){right = newR;}
+protected:
+    Node* left;
+    Node* right;
+};
 
 class ExpressionTree{
 public:
-    double CalculateValue(Node* root);
+    ExpressionTree(Node* newRoot){
+        root = newRoot;
+    }
+    double CalculateValue(){
+        return root->calculateValue();
+    }
 private:
     Node* root;
 };
 
-class Node{
+class OperatorNode: public Node{
 public:
-
+    OperatorNode(char newVal = ' '){ val = newVal;}
+    virtual double calculateValue(){
+        switch (val) {
+            case '+':
+                return ((left->calculateValue())+(right->calculateValue()));
+            case '-':
+                return ((left->calculateValue())-(right->calculateValue()));
+            case '*':
+                return ((left->calculateValue())*(right->calculateValue()));
+            case '/':
+                return ((left->calculateValue())/(right->calculateValue()));
+        }
+    }
 private:
-    int num;
-    char opt;
+    char val;
 };
 
-int main(){
+class OperandNode: public Node{
+public:
+    OperandNode(int newVal = 0){val = newVal;}
+    virtual double calculateValue(){
+        return val;
+    }
+private:
+    int val;
+};
+
+
+int main() {
+    Node* root = new OperatorNode('*');
+    root->setLeft(new OperatorNode('+'));
+    root->setRight(new OperandNode(7));
+    root->getLeft()->setLeft(new OperandNode(2));
+    root->getLeft()->setRight(new OperandNode(3));
+
+
+    ExpressionTree t(root);
+    cout<<t.CalculateValue()<<endl;
 
     return 0;
 }
+
